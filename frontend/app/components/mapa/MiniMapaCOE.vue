5609b246f615335518f61344aa6bfe2419f10f7f
EJCP3 <euddycabrera03@gmail.com>
2026-09-06 00:06:53 -0400

    style(mapa): remover borde activo y de enfoque en provincias

--- frontend/app/components/mapa/MiniMapaCOE.vue
+++ frontend/app/components/mapa/MiniMapaCOE.vue
@@ -258,13 +258,13 @@
               :id="'prov-path-' + prov.code"
               :d="prov.path"
               :fill="getProvinceFill(prov)"
-              class="origin-center transition-all duration-200 cursor-pointer stroke-transparent [stroke-width:0px]"
+              class="origin-center transition-all duration-200 cursor-pointer stroke-transparent [stroke-width:0px] focus:outline-none outline-none"
               :class="[
                 selectedProvince?.code === prov.code
-                  ? 'brightness-110 drop-shadow-xl !stroke-slate-950 ![stroke-width:2px]'
+                  ? 'brightness-110 drop-shadow-md'
                   : isProvinceDimmed(prov.alerta)
                     ? 'opacity-25 saturate-50'
-                    : 'opacity-100 hover:brightness-105 hover:!stroke-slate-900/60 hover:![stroke-width:1.5px]'
+                    : 'opacity-100 hover:brightness-105'
               ]"
               stroke-linejoin="round"
               stroke-linecap="round"
